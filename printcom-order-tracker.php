b1966fc950ffa1d9e472944ae9f9e6eac1d4c680
Rubenrikk <Rubenrikk@gmail.com>
2025-09-13 11:13:52 +0200

    Update 1.7.3
    tekst aangepast

--- printcom-order-tracker.php
+++ printcom-order-tracker.php
@@ -2,7 +2,7 @@
 /**
  * Plugin Name: Print.com Order Tracker (Track & Trace Pagina's)
  * Description: Maakt per ordernummer automatisch een track & trace pagina aan en toont live orderstatus, items en verzendinformatie via de Print.com API. Tokens worden automatisch vernieuwd. Divi-vriendelijk.
- * Version:     1.7.2
+ * Version:     1.7.3
  * Author:      RikkerMediaHub
  * License:     GNU GPLv2
  * Text Domain: printcom-order-tracker
